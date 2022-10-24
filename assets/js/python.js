66843087b6449cd6e6e4c2bb0a0b116e20ce105e
Clyoucker <robbikriper@gmail.com>
2022-10-25 00:08:29 +0300

    Update Code: Python.js New: int,str,bool

--- assets/js/python.js
+++ assets/js/python.js
@@ -1,5 +1,12 @@
 const print = (message) => {console.log(message)}
 const type = (value) => {return typeof(value)}
+const int = (value) => {return value = Number(value)}
+const str = (value) => {return value = String(value)}
+const bool = (value) => {
+    if (value === true){return false}
+    else if (value === false){return true}
+    else {return NaN}
+}
 const is = (value, bool) => {
     if (value === bool){
         return true;
@@ -11,9 +18,8 @@ const is = (value, bool) => {
         return false;
     }
 }
-const for_in = (value,values) => {
 
-}
+
 
 
 
